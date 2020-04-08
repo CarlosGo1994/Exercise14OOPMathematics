efb72a8479825ab38087c7e9ebb8ff806e99a72d
CarlosGo1994 <charls21ronald@gmail.com>
2020-04-08 11:13:59 -0500

    OOPMathematics

---
+++ src/com/exercise14oopmathemarics/app/MatheramticsApp.java
@@ -0,0 +1,27 @@
+package com.exercise14oopmathemarics.app;
+
+import java.util.Scanner;
+
+public class MatheramticsApp {
+
+	public static void main(String[] args) {
+		// variables declaration 
+		double base=0;
+		double height=0;
+		double radio=0;
+		double side=0;
+		double result=0;
+		
+		//Objects construccion
+		Scanner input = new Scanner(System.in);
+		MatheramiticOperations myOperations = new MathematicsOperation();
+		
+		
+		System.out.println("Input the base");
+		base = input.nextDouble();
+		
+		
+
+	}
+
+}
---
+++ src/com/exercise14oopmathemarics/model/MathematicsOperations.java
@@ -0,0 +1,12 @@
+package com.exercise14oopmathemarics.model;
+
+public class MathematicsOperations {
+	
+	final double PI=3.1416
+	
+	public double areaTriangle(double base, double height)
+	{
+		return (base*height)/2;
+	}
+
+}
